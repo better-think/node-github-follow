912c6dbdc4c56e4f5ae25a4a4ba86ff7c3789fa8
chaojin752 <chaojin752@gmail.com>
2021-03-30 02:45:20 +0300

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+// const twilioNum = process.env.TWILIO_PHONE_NUMBER;
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cookieParser());
+
+app.listen(process.env.PORT || 8888);
+console.log("server listening port=", process.env.PORT || 8888);
