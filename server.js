70db356009e3a6918fe514971541376ef6da3ee1
chaojin752 <chaojin752@gmail.com>
2021-03-30 02:47:35 +0300

    delete comment

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-// const twilioNum = process.env.TWILIO_PHONE_NUMBER;
 
 const app = express();
 app.use(express.json());
